af2b70106545c60b4552fd048fac752c25a134a6
kseniaPXL <ksenia.perkhurova@student.pxl.be>
2021-09-29 22:19:03 +0200

    Add project files.

---
+++ SEng/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEng
+{
+    public class Student
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string studentNumber;
+        private string group;
+        public string Group { get; set; }
+
+        public Student(string firstName, string lastName, string email, string studentNumber, string group ):
+            this(firstName, lastName, email,studentNumber)
+        {
+            this.group = group;
+        }
+
+        public Student(string firstName, string lastName, string email, string studentNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.studentNumber = studentNumber;
+            this.group = null;
+        }
+
+
+    }
+}
---
+++ SEng/Students.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEng
+{
+    public class Students
+    {
+        private IList<Student> allStudents = new List<Student>();
+        public IList<Student> ListOfStudents { get; }
+        public void AddStudent(Student student)
+        {
+            allStudents.Add(student);
+        }
+        public IList<Student> GetAllStudentsWhithGroup()
+        {
+            return null;
+        }
+        public IList<Student> GetAllStudentsWhitouthGroup()
+        {
+            return null;
+        }
+    }
+}
